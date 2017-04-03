269b2b1bd6db303b26450ee8468580d3d16e3fa3
Mike Feijs <mike@mijnwebwinkel.nl>
2017-04-03 15:46:37 +0200

    Requests should implement getters and setters for default gateway parameters. Added tests for Gateway

--- tests/GatewayTest.php
+++ tests/GatewayTest.php
@@ -2,7 +2,10 @@
 
 namespace MyOnlineStore\Tests\Omnipay\KlarnaCheckout;
 
+use Klarna\Rest\Transport\ConnectorInterface;
 use MyOnlineStore\Omnipay\KlarnaCheckout\Gateway;
+use MyOnlineStore\Omnipay\KlarnaCheckout\Message\AuthorizeRequest;
+use MyOnlineStore\Omnipay\KlarnaCheckout\Message\FetchTransactionRequest;
 use Omnipay\Tests\GatewayTestCase;
 
 class GatewayTest extends GatewayTestCase
@@ -15,5 +18,36 @@ class GatewayTest extends GatewayTestCase
     protected function setUp()
     {
         $this->gateway = new Gateway($this->getHttpClient(), $this->getHttpRequest());
+    }
+
+    public function testInitialisationForUSRegion()
+    {
+        $this->gateway->initialize([
+            'api_region' => Gateway::API_VERSION_NORTH_AMERICA,
+            'testMode' => false
+        ]);
+        self::assertEquals(
+            ConnectorInterface::NA_BASE_URL,
+            $this->gateway->getParameter('connector')->getClient()->getBaseUrl()
+        );
+
+        $this->gateway->initialize([
+            'api_region' => Gateway::API_VERSION_NORTH_AMERICA,
+            'testMode' => true
+        ]);
+        self::assertEquals(
+            ConnectorInterface::NA_TEST_BASE_URL,
+            $this->gateway->getParameter('connector')->getClient()->getBaseUrl()
+        );
+    }
+
+    public function testAuthorize()
+    {
+        $this->assertInstanceOf(AuthorizeRequest::class, $this->gateway->authorize());
+    }
+
+    public function testFetchTransaction()
+    {
+        $this->assertInstanceOf(FetchTransactionRequest::class, $this->gateway->fetchTransaction());
     }
 }
