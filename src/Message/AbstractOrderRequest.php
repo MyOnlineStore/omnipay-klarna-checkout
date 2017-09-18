32792b2d2f2c8426ab99a545dca68bd17c849053
Fred-Jan van der Eijken <fred-jan@vandereijken.nl>
2017-09-18 16:21:32 +0200

    Added GUI + merchant reference parameters to the requests creating/updating Klarna orders

--- src/Message/AbstractOrderRequest.php
+++ src/Message/AbstractOrderRequest.php
@@ -9,6 +9,38 @@ abstract class AbstractOrderRequest extends AbstractRequest
     use ItemDataTrait;
 
     /**
+     * @return Address
+     */
+    public function getBillingAddress()
+    {
+        return $this->getParameter('billing_address');
+    }
+
+    /**
+     * @return bool
+     */
+    public function getGuiAutofocus()
+    {
+        return $this->getParameter('gui_autofocus');
+    }
+
+    /**
+     * @return bool
+     */
+    public function getGuiMinimalConfirmation()
+    {
+        return $this->getParameter('gui_minimal_confirmation');
+    }
+
+    /**
+     * @return Address
+     */
+    public function getShippingAddress()
+    {
+        return $this->getParameter('shipping_address');
+    }
+
+    /**
      * @param array $billingAddress
      *
      * @return $this
@@ -16,6 +48,30 @@ abstract class AbstractOrderRequest extends AbstractRequest
     public function setBillingAddress($billingAddress)
     {
         $this->setParameter('billing_address', Address::fromArray($billingAddress));
+
+        return $this;
+    }
+
+    /**
+     * @param bool $value
+     *
+     * @return $this
+     */
+    public function setGuiAutofocus($value)
+    {
+        $this->setParameter('gui_autofocus', $value);
+
+        return $this;
+    }
+
+    /**
+     * @param bool $value
+     *
+     * @return $this
+     */
+    public function setGuiMinimalConfirmation($value)
+    {
+        $this->setParameter('gui_minimal_confirmation', $value);
 
         return $this;
     }
@@ -33,27 +89,18 @@ abstract class AbstractOrderRequest extends AbstractRequest
     }
 
     /**
-     * @return Address
-     */
-    public function getBillingAddress()
-    {
-        return $this->getParameter('billing_address');
-    }
-
-    /**
-     * @return Address
-     */
-    public function getShippingAddress()
-    {
-        return $this->getParameter('shipping_address');
-    }
-
-    /**
      * @return array
      */
     protected function getOrderData()
     {
-        $data = [];
+        $data = [
+            'locale' => str_replace('_', '-', $this->getLocale()),
+            'order_amount' => $this->getAmountInteger(),
+            'order_tax_amount' => $this->toCurrencyMinorUnits($this->getTaxAmount()),
+            'order_lines' => $this->getItemData($this->getItems()),
+            'purchase_country' => explode('_', $this->getLocale())[1],
+            'purchase_currency' => $this->getCurrency(),
+        ];
 
         if (null !== $shippingAddress = $this->getShippingAddress()) {
             $data['shipping_address'] = $shippingAddress->getArrayCopy();
@@ -63,16 +110,28 @@ abstract class AbstractOrderRequest extends AbstractRequest
             $data['billing_address'] = $billingAddress->getArrayCopy();
         }
 
-        return array_merge(
-            $data,
-            [
-                'locale' => str_replace('_', '-', $this->getLocale()),
-                'order_amount' => $this->getAmountInteger(),
-                'order_tax_amount' => $this->toCurrencyMinorUnits($this->getTaxAmount()),
-                'order_lines' => $this->getItemData($this->getItems()),
-                'purchase_country' => explode('_', $this->getLocale())[1],
-                'purchase_currency' => $this->getCurrency(),
-            ]
-        );
+        if (null !== $merchantReference1 = $this->getMerchantReference1()) {
+            $data['merchant_reference1'] = $this->getMerchantReference1();
+        }
+
+        if (null !== $merchantReference2 = $this->getMerchantReference2()) {
+            $data['merchant_reference2'] = $this->getMerchantReference2();
+        }
+
+        $guiOptions = [];
+
+        if (false === $this->getGuiAutofocus()) {
+            $guiOptions[] = 'disable_autofocus';
+        }
+
+        if ($this->getGuiMinimalConfirmation()) {
+            $guiOptions[] = 'minimal_confirmation';
+        }
+
+        if (!empty($guiOptions)) {
+            $data['gui'] = ['options' => $guiOptions];
+        }
+
+        return $data;
     }
 }
