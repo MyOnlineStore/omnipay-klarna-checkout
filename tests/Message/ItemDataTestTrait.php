8ce03e9b8e3e8bfcbe320d080107d19f7a94ed9c
Marleen Berg <marleen@mijnwebwinkel.nl>
2018-02-01 10:14:59 +0100

    fixed tests

--- tests/Message/ItemDataTestTrait.php
+++ tests/Message/ItemDataTestTrait.php
@@ -12,6 +12,7 @@ trait ItemDataTestTrait
     protected function getItemMock()
     {
         $item = \Mockery::mock(ItemInterface::class);
+        $item->shouldReceive('getType')->andReturn('shipping_fee');
         $item->shouldReceive('getName')->andReturn('item-name');
         $item->shouldReceive('getQuantity')->andReturn(1);
         $item->shouldReceive('getTaxRate')->andReturn(20.0);
@@ -28,6 +29,7 @@ trait ItemDataTestTrait
     protected function getExpectedOrderLine()
     {
         return [
+            'type' => 'shipping_fee',
             'name' => 'item-name',
             'quantity' => 1,
             'tax_rate' => 2000,
